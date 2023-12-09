e5676e1e492767270f202eb90c30a5eb2a0e1d20
Alexandre CASU <99054716+Aldrex99@users.noreply.github.com>
2023-12-09 15:43:34 +0100

    Refactoring error validation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { IRequestUser } from "../types/user.type";
 import * as userService from "../services/user.service";
 import * as formatUser from "../utils/formatUser.util";
 import { validationResult } from "express-validator";
+import { validationErrorsUtil } from "../utils/validationErrors.util";
 
 // Get profile of the user
 export const getProfile = async (req: IRequestUser, res: Response, next: NextFunction) => {
@@ -27,14 +28,8 @@ export const getProfile = async (req: IRequestUser, res: Response, next: NextFun
 // Update user
 export const updateUser = async (req: IRequestUser, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      code: 422,
-      message: "Les données envoyées sont incorrectes",
-      errors: errors.array(),
-    });
-  }
-  
+  await validationErrorsUtil(errors, res);
+
   try {
     // Get user
     const user = await userService.getUserById(req.user.id);
